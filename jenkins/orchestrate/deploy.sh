d3fc5fdaa5b25c6d0748cc520a10ed44fa1e7a4f
Martin Michalec <martin@michalec.dev>
2026-03-10 08:02:23 +0300

    [jenkins/orchestrate] delete instance if failed

--- jenkins/orchestrate/deploy.sh
+++ jenkins/orchestrate/deploy.sh
@@ -3,13 +3,16 @@
 set -xeu
 
 command="create"
-if openstack stack show "$STACK_NAME" >/dev/null 2>&1; then
+if openstack stack show "${STACK_NAME}" >/dev/null 2>&1; then
     command="update"
 fi
 
-exec openstack stack ${command} --wait -t jenkins/orchestrate/heat.yaml \
-  --parameter "image=${IMAGE}"                                          \
-  --parameter "flavor=${FLAVOR}"                                        \
-  --parameter "key_name=${KEY_NAME}"                                    \
-  --parameter "network=${NETWORK}"                                      \
-  ${STACK_NAME}
+if ! openstack stack ${command} --wait -t jenkins/orchestrate/heat.yaml \
+     --parameter "image=${IMAGE}"                                       \
+     --parameter "flavor=${FLAVOR}"                                     \
+     --parameter "key_name=${KEY_NAME}"                                 \
+     --parameter "network=${NETWORK}"                                   \
+     "${STACK_NAME}"; then
+    openstack stack delete "${STACK_NAME}"
+    exit 1
+fi
