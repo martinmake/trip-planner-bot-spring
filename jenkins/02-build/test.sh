6ed9bbdc2e524243c034b7c182d1fc539750304f
Martin Michalec <martin@michalec.dev>
2026-04-21 02:01:10 +0300

    [docker-compose] fix sockets

--- jenkins/02-build/test.sh
+++ jenkins/02-build/test.sh
@@ -7,5 +7,5 @@ podman compose up -d --build
 sleep 30
 podman compose ps
 podman compose logs --tail=50 app
-curl -f http://localhost:8080/healthcheck || echo "Healthcheck failed"
+curl -f http://localhost:8088/healthcheck || echo "Healthcheck failed"
 podman compose down -v
