c9ed65764c4ed4ae082754bdb23ba44e32418b7b
Martin Michalec <martin@michalec.dev>
2026-04-21 10:19:37 +0300

    [jenkins] move tripplanner.service installation path

--- jenkins/04-deploy/remote.sh
+++ jenkins/04-deploy/remote.sh
@@ -4,7 +4,7 @@ set -xeu
 
 sudo install -D --target-directory /usr/local/lib/tripplanner/ /tmp/app.jar
 sudo install -D --target-directory /etc/tripplanner/ /tmp/env
-sudo install -D --target-directory /usr/local/lib/systemd/system/ /tmp/tripplanner.service
+sudo install -D --target-directory /etc/systemd/system/ /tmp/tripplanner.service
 sudo install -d /var/lib/tripplanner
 
 sudo systemctl daemon-reload
