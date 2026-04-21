a167f22dd6c52a89c5e782b9b178ffd90e28de9c
Martin Michalec <martin@michalec.dev>
2026-04-21 03:44:20 +0300

    [jenkins] synchronize systemd service declarations

--- jenkins/04-deploy/remote.sh
+++ jenkins/04-deploy/remote.sh
@@ -5,5 +5,6 @@ set -xeu
 sudo install -D --target-directory /usr/local/lib/tripplanner/ /tmp/app.jar
 sudo install -D --target-directory /etc/tripplanner/ /tmp/env
 sudo install -D --target-directory /usr/local/lib/systemd/system/ /tmp/tripplanner.service
+sudo install -d /var/lib/tripplanner
 
 sudo systemctl restart tripplanner
